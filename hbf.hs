f5e3cb87407a06b49538067fb5406b3cc52b38ba
takatoh <takatoh.m@gmail.com>
2006-09-07 15:57:37 +0000

    Initial import.
    git-svn-id: file:///cygdrive/u/svn/hbf@1 019b627c-319b-b14b-95e0-79cc223515dc

---
+++ hbf.hs
@@ -0,0 +1,28 @@
+data BrainFuck = BF { bfPointer :: Int, bfValues :: [Integer] } deriving (Show)
+
+
+bfInitial :: BrainFuck
+bfInitial = BF { bfPointer = 0, bfValues = [0,0,0,0,0,0,0,0,0,0] }
+
+
+bfValue :: BrainFuck -> Integer
+bfValue bf = (bfValues bf) !! (bfPointer bf)
+
+
+bfIncrement :: BrainFuck -> BrainFuck
+bfIncrement (BF p v) = BF p ((take p v) ++ [(v !! p) + 1] ++ (tail $ drop p v))
+
+
+bfDecrement :: BrainFuck -> BrainFuck
+bfDecrement (BF p v) = BF p ((take p v) ++ [(v !! p) - 1] ++ (tail $ drop p v))
+
+
+bfShift :: BrainFuck -> BrainFuck
+bfShift (BF p v) = BF (p+1) v
+
+
+bfUnshift :: BrainFuck -> BrainFuck
+bfUnshift (BF p v) = BF (p-1) v
+
+
+
