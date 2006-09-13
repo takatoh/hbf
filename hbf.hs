649ded2d8c555b0dadaf6335c15725367319f0a9
takatoh <takatoh.m@gmail.com>
2006-09-13 11:30:47 +0000

    hbf.hs (bfPrint): New. support output.
    git-svn-id: file:///cygdrive/u/svn/hbf@10 019b627c-319b-b14b-95e0-79cc223515dc

--- hbf.hs
+++ hbf.hs
@@ -2,6 +2,7 @@ module Main where
 
 
 import System
+import Monad
 
 
 data BrainF_ck = BF { bfPointer :: Int, bfRegister :: [Int] } deriving (Show)
@@ -31,25 +32,26 @@ bfUnshift :: BrainF_ck -> BrainF_ck
 bfUnshift (BF p v) = BF (p-1) v
 
 
-bfInput :: BrainF_ck -> Int -> BrainF_ck
-bfInput (BF p v) n = BF p ((take p v) ++ [n] ++ (tail $ drop p v))
+--bfInput :: BrainF_ck -> Int -> BrainF_ck
+--bfInput (BF p v) n = BF p ((take p v) ++ [n] ++ (tail $ drop p v))
+--
 
-
-bfPrint bf = do print $ bfValue bf
+bfPrint bf = do putStr $ show $ bfValue bf
                 return bf
 
 
-bfEvaluate bf c = do bf' <- bf
-                     case c of '+' -> return $ bfIncrement bf'
-                               '-' -> return $ bfDecrement bf'
-                               '>' -> return $ bfShift bf'
-                               '<' -> return $ bfUnshift bf'
-                               '.' -> bfPrint bf'
+bfEvaluate :: BrainF_ck -> Char -> IO BrainF_ck
+bfEvaluate bf c = case c of '+' -> return $ bfIncrement bf
+                            '-' -> return $ bfDecrement bf
+                            '>' -> return $ bfShift bf
+                            '<' -> return $ bfUnshift bf
+                            '.' -> bfPrint bf
 
 
 
---main = do args <- getArgs
---          prog <- readFile $ head args
---          bf <- foldl bfEvaluate bfInitial prog
---          print bf
---
+--main :: IO BrainF_ck
+main = do args <- getArgs
+          prog <- readFile $ head args
+          foldM bfEvaluate bfInitial prog
+
+
