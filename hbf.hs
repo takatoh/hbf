a9670126fada65bfda24583cd502c63c8165058b
takatoh <takatoh.m@gmail.com>
2006-09-16 12:54:54 +0000

    hbf.hs: Implement `[' and `]'.
    git-svn-id: file:///cygdrive/u/svn/hbf@15 019b627c-319b-b14b-95e0-79cc223515dc

--- hbf.hs
+++ hbf.hs
@@ -7,6 +7,8 @@ import Monad
 
 data BrainF_ck = BF { bfPointer :: Int, bfRegister :: [Int] } deriving (Show)
 
+type Program = ([Char], [Char])
+
 
 bfInitial :: BrainF_ck
 bfInitial = BF { bfPointer = 0, bfRegister = [0,0,0,0,0,0,0,0,0,0] }
@@ -54,11 +56,46 @@ bfEvaluate bf '.' = bfPrint bf
 bfEvaluate bf ',' = bfInput bf
 
 
+progNew :: String -> Program
+progNew str = ([], str)
 
-main :: IO ()
+
+progFetch :: Program -> [Char]
+progFetch prog = snd prog
+
+
+progShift :: Program -> Program
+progShift (l, (r:rs)) = (l ++ [r], rs)
+
+
+progUnshift :: Program -> Program
+progUnshift (l, r) = (take ((length l) -1) l, (last l):r)
+
+
+progSkip :: BrainF_ck -> Program -> Program
+progSkip bf prog = if bfValue bf == 0 then skip prog else progShift prog
+  where skip p = if (head $ progFetch next) == ']' then next else skip next
+          where next = progShift p
+
+
+progBack :: BrainF_ck -> Program -> Program
+progBack bf prog = if bfValue bf == 0 then progShift prog else back prog
+  where back p = if (head $ progFetch prev) == '[' then prev else back prev
+          where prev = progUnshift p
+
+
+bfRun :: BrainF_ck -> Program -> IO BrainF_ck
+bfRun bf prog = run $ progFetch prog
+  where run []                  = return bf
+        run (c:cs) | c == '['   = bfRun bf (progSkip bf prog)
+                   | c == ']'   = bfRun bf (progBack bf prog)
+                   | otherwise = do next <- bfEvaluate bf c
+                                    bfRun next (progShift prog)
+
+
+main :: IO BrainF_ck
 main = do args <- getArgs
-          prog <- readFile $ head args
-          result <- foldM bfEvaluate bfInitial prog
-          print result
+          src <- readFile $ head args
+          bfRun bfInitial $ progNew $ concat $ lines src
 
 
